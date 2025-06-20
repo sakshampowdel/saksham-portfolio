e7e731ef0a0bea998ea1b099c28da82d91170872
Saksham Powdel <sakshampowdel@gmail.com>
2025-06-19 23:10:21 -0400

    refactor(frontend): change buttons to lists and a hrefs

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,22 +4,39 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="max-sm:flex-col flex justify-between font-bold p-10 text-xl gap-5">
-      <button>Saksham Powdel</button>
-      <div className="max-sm:hidden flex gap-5">
-        <button>About Me</button>
-        <button>Projects</button>
-        <button>Contact Me</button>
-      </div>
-      <button onClick={() => setIsOpen(!isOpen)} className="sm:hidden">
+    <div className="max-sm:flex-col flex justify-between font-bold p-10 text-xl gap-5 text-center">
+      <a href="/">Saksham Powdel</a>
+
+      <ul className="max-sm:hidden flex gap-5">
+        <li>
+          <a href="#about">About Me</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Me</a>
+        </li>
+      </ul>
+
+      <button
+        className="sm:hidden cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         Menu
       </button>
       {isOpen && (
-        <div className="sm:hidden flex flex-col gap-5">
-          <button>About Me</button>
-          <button>Projects</button>
-          <button>Contact Me</button>
-        </div>
+        <ul className="sm:hidden flex flex-col gap-5">
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+        </ul>
       )}
     </div>
   );
