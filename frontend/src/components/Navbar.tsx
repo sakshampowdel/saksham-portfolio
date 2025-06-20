92a3bb4e68d3c34c81996b1fa641d7be04d84a18
Saksham Powdel <sakshampowdel@gmail.com>
2025-06-19 22:55:54 -0400

    feat(frontend): create minimal responsive navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 function Navbar() {
-  return <></>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="max-sm:flex-col flex justify-between font-bold p-10 text-xl gap-5">
+      <button>Saksham Powdel</button>
+      <div className="max-sm:hidden flex gap-5">
+        <button>About Me</button>
+        <button>Projects</button>
+        <button>Contact Me</button>
+      </div>
+      <button onClick={() => setIsOpen(!isOpen)} className="sm:hidden">
+        Menu
+      </button>
+      {isOpen && (
+        <div className="sm:hidden flex flex-col gap-5">
+          <button>About Me</button>
+          <button>Projects</button>
+          <button>Contact Me</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Navbar;
