7e7775f0933701c6f05e16b76d06937cbdd7017a
Saksham Powdel <sakshampowdel@gmail.com>
2025-08-14 22:46:05 -0400

    chore: start over

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,5 @@
-import Footer from "./pages/Footer";
-import Hero from "./pages/landing/Hero";
-import Navbar from "./pages/Navbar";
-import Projects from "./pages/landing/Projects";
-import Contact from "./pages/landing/Contact";
-
 function App() {
-  return (
-    <div className="">
-      <Navbar />
-      <Hero />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  );
+  return <div className=""></div>;
 }
 
 export default App;
