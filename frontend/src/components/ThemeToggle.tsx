999c1fe6e673275b616b9af09d13d295c9883d1f
Saksham Powdel <sakshampowdel@gmail.com>
2025-08-15 18:36:18 -0400

    feat: add theme choice to localstorage

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -2,11 +2,15 @@ import { useState, useEffect } from "react";
 import { Sun, Moon } from "lucide-react";
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem("theme") || "light"
+  );
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
+
   return (
     <div>
       <button
