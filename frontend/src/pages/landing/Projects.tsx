625a1983e225c6c125258c2ca6d39cc558733811
Saksham Powdel <sakshampowdel@gmail.com>
2025-08-16 19:40:59 -0400

    style: nevermind those shadows hurt eyes

--- frontend/src/pages/landing/Projects.tsx
+++ frontend/src/pages/landing/Projects.tsx
@@ -7,7 +7,7 @@ const projectList: ProjectType[] = data;
 const Projects = () => {
   return (
     <section id="projects" className="py-12 md:px-24 min-h-screen scroll-mt-24">
-      <h1 className="text-6xl max-md:text-5xl font-bold text-accent text-center pb-12 text-shadow-lg">
+      <h1 className="text-6xl max-md:text-5xl font-bold text-accent text-center pb-12">
         Projects
       </h1>
       <div className="space-y-4">
