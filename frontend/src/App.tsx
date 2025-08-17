7b3378e087f124b6a9f3454851f83f332d9a91a7
Saksham Powdel <sakshampowdel@gmail.com>
2025-08-16 20:21:57 -0400

    feat: add contact section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./pages/landing/Hero";
 import About from "./pages/landing/About";
 import Experience from "./pages/landing/Experience";
 import Projects from "./pages/landing/Projects";
+import Contact from "./pages/landing/Contact";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <About />
       <Experience />
       <Projects />
+      <Contact />
     </div>
   );
 }
